8f5ee09db5d762c18f4b6c929bef65cc7e803121
Lukasz <lukasz@siwiec.com.pl>
2019-09-22 17:35:07 +0200

    npm scripts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -10,6 +11,13 @@ app.use(bodyParser.json());
 const taskRoutes = require("./routes/task");
 
 app.use("/api/task", taskRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = 5000;
 mongoose
