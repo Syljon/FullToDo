6324d0816b5715eb54dea12ce2b7e1d6781193ab
Lukasz <lukasz@siwiec.com.pl>
2019-09-22 17:55:28 +0200

    heroku test

--- server.js
+++ server.js
@@ -12,12 +12,13 @@ const taskRoutes = require("./routes/task");
 
 app.use("/api/task", taskRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// Server static assets if in production
+// Set static folder
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = 5000;
 mongoose
